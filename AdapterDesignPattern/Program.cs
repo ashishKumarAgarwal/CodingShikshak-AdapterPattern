2c136c3e3e96a1a78b19e13cf775558f4a3ffed8
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-08-16 11:37:07 +0530

    Added empty console project

---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
